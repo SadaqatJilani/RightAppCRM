be64a5820661e8e859f0994db2dc052aa5ec6b50
Sadaqat Jeelani <sadaqat@zeptosystems.com>
2018-01-11 11:38:59 +0500

    Android Drawer Layout not completed

---
+++ RightCRM.Droid/Fragments/Help/HelpFeedbackFragment.cs
@@ -0,0 +1,21 @@
+﻿using Android.Runtime;
+using RightCRM.Core.ViewModels;
+using Android.Views;
+using Android.OS;
+using MvvmCross.Droid.Shared.Attributes;
+
+namespace RightCRM.Droid.Fragments
+{
+	[MvxFragment(typeof(MainViewModel), Resource.Id.content_frame)]
+    [Register("xplatformmenus.droid.fragments.HelpFeedbackFragment")]
+    public class HelpFeedbackFragment : BaseFragment<HelpAndFeedbackViewModel>
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            ShowHamburgerMenu = true;
+            return base.OnCreateView(inflater, container, savedInstanceState);
+        }
+
+        protected override int FragmentId => Resource.Layout.fragment_helpfeedback;
+    }
+}
---
+++ RightCRM.Droid/Fragments/Settings/SettingsFragment.cs
@@ -0,0 +1,21 @@
+﻿using Android.Runtime;
+using RightCRM.Core.ViewModels;
+using Android.Views;
+using Android.OS;
+using MvvmCross.Droid.Shared.Attributes;
+
+namespace RightCRM.Droid.Fragments
+{
+    [MvxFragment(typeof(MainViewModel), Resource.Id.content_frame)]
+    [Register("xplatformmenus.droid.fragments.SettingsFragment")]
+    public class SettingsFragment : BaseFragment<SettingsViewModel>
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            ShowHamburgerMenu = true;
+            return base.OnCreateView(inflater, container, savedInstanceState);
+        }
+
+        protected override int FragmentId => Resource.Layout.fragment_settings;
+    }
+}
